f5e55ba1ce84e2d52583a724aa90a032cc779402
Kamran Ali <km775.ali@gmail.com>
2024-07-05 12:53:27 +0500

    react icon added

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,12 +1,19 @@
 import React from "react";
 import { Typewriter } from "react-simple-typewriter";
-import ParticleBackground from "./ParticleBackground"; // Import the ParticleBackground component
-import "./Header.css"; // Import the CSS file for additional styling if needed
+import {
+  FaGithub,
+  FaLinkedin,
+  FaTwitter,
+  FaWhatsapp,
+  FaFacebook,
+  FaInstagram,
+} from "react-icons/fa";
+
+import "./Header.css";
 
 const Header = () => {
   return (
-    <div className="header-container h-screen flex flex-col justify-center items-center relative">
-      <ParticleBackground /> {/* Add the ParticleBackground component */}
+    <div className="header-container h-[80vh] flex flex-col justify-center items-center relative">
       <div className="bg-white bg-opacity-80 p-10 rounded-lg text-center shadow-lg relative z-10">
         <h4 className="text-2xl font-bold pb-5 tracking-widest text-black">
           Welcome
@@ -29,6 +36,14 @@ const Header = () => {
             />
           </span>
         </h3>
+      </div>
+      <div className="flex gap-8 mt-5 z-50">
+        <FaGithub size={30} className="text-gray-700 hover:text-gray-900" />
+        <FaLinkedin size={30} className="text-gray-700 hover:text-gray-900" />
+        <FaTwitter size={30} className="text-gray-700 hover:text-gray-900" />
+        <FaWhatsapp size={30} className="text-gray-700 hover:text-gray-900" />
+        <FaFacebook size={30} className="text-gray-700 hover:text-gray-900" />
+        <FaInstagram size={30} className="text-gray-700 hover:text-gray-900" />
       </div>
     </div>
   );
