050e3d2996e5ab891f8a5ff2f855c8d742d27ccf
Kamran Ali <km775.ali@gmail.com>
2024-07-05 11:12:36 +0500

    navbar implemented

--- src/components/navbar/index.jsx
+++ src/components/navbar/index.jsx
@@ -1,7 +1,21 @@
 import React from "react";
 
 const Header = () => {
-  return <div>Navbar</div>;
+  return (
+    <div className=" bg-white flex justify-between items-center px-10 py-5">
+      <div className=" text-2xl uppercase font-bold tracking-[0.3em]">
+        Kamran Ali
+      </div>
+      <div className=" flex gap-4 font-semibold">
+        <h4>Home</h4>
+        <h4>About</h4>
+        <h4>Services</h4>
+        <h4>Experience</h4>
+        <h4>Work</h4>
+        <h4>Contact</h4>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
